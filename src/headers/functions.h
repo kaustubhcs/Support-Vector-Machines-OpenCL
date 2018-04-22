d1d00ed2bd2cbb19acbe44e5e94a75a62b548344
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2018-04-22 02:13:52 -0400

    Code Update

--- src/headers/functions.h
+++ src/headers/functions.h
@@ -15,21 +15,5 @@
 #include <cmath>
 #include <fstream>
 
-struct MemoryRegion
-{
-  void *startAddr;
-  void *endAddr;
-  int isReadable;
-  int isWriteable;
-  int isExecutable;
-  unsigned long blockSize;
-};
-
-int find_number_of_lines (const std::string& );
-void fill_mr_struct(const std::string&, int, MemoryRegion *);
-int pos_of_line (const std::string&, int );
-int find_delimiter(const std::string&, int);
-unsigned long long int string2long (const std::string&);
-
 
 #endif // FUNCTIONS_H
